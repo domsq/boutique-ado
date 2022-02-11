99a2de1745382ef1135bf24d79911f980344370d
domsq <Domsq@hotmail.com>
2022-02-11 18:34:01 +0000

    Create userprofile form, update profile template and view

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -9,9 +10,13 @@ def profile(request):
     """
     userprofile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=userprofile)
+    orders = userprofile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': userprofile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
